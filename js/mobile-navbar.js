d9f46dcc87613ad2a00b7d24be55441ff6c097bb
ramonroch <86085266+ramonroch@users.noreply.github.com>
2022-04-06 16:51:55 -0300

    add bootstrap

--- js/mobile-navbar.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementsByClassName('mobile-menu');
-
-function toggleMenu(){
-const nav = document.getElementsByClassName('navbar-content');
-navigator.classList.toggle('active')
-
-}
-
-menu.addEventListener('click', toggleMenu)
